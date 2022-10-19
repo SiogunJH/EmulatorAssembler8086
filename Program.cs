77223d54980d3f3881026df54506a536cd0ab5dc
JakubH <homarofficial@gmail.com>
2022-10-20 00:11:29 +0200

    Sample CS Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace System
+{
+    class Hello
+    {
+        static void Main()
+        {
+            string userInput;
+            do
+            {
+                userInput = Console.ReadLine();
+                Console.WriteLine($"Recieved: {userInput}");
+            } while (userInput != "~quit");
+        }
+    }
+}
