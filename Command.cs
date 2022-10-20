ea5bebb78150df84b375ee5a2a43d85e2b73314e
JakubH <homarofficial@gmail.com>
2022-10-21 01:13:44 +0200

    UPDATE - Create console and await commands

---
+++ Command.cs
@@ -0,0 +1,29 @@
+﻿namespace System
+{
+    class Command
+    {
+        public static void Recognize(string command)
+        {
+            //Prepare command for analyze
+            command = command.Trim();
+            command = command.Split(";")[0];
+            //No response nor action
+            if (command == "" || command == null)
+                return;
+
+            //Detect command
+            string[] commandArray = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            switch (commandArray[0])
+            {
+                case "AAA":
+
+                    break;
+                default:
+                    Console.WriteLine($"Command '{commandArray[0]}' was not recognized - make sure there are no typos and try again");
+                    break;
+            }
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
 ﻿namespace System
 {
-    class Hello
+    class Base
     {
         static void Main()
         {
+            //Welcome message
+            Console.WriteLine("Welcome to 8086 Simulator made by Hojda");
+            Console.WriteLine("Here is a list of meta-commands available:\n");
+            Console.WriteLine("~quit - exits the simulator");
+            Console.WriteLine("");
+
+            //Program
             string userInput;
             do
             {
+                Console.Write("> ");
                 userInput = Console.ReadLine();
-                Console.WriteLine($"Recieved: {userInput}");
+
+                Command.Recognize(userInput);
+
             } while (userInput != "~quit");
         }
     }
