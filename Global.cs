0454e7599de877c2dfc68b9177711ac444a615f0
JakubH <homarofficial@gmail.com>
2022-10-22 15:35:44 +0200

    Flag Storage and Flag Commands

---
+++ Global.cs
@@ -0,0 +1,11 @@
+namespace System
+{
+    class Global
+    {
+        public static int Register; //TODO
+        public static int Segments; //TODO
+        public static int Pointers; //TODO
+        public static Flags Flags;
+        public static int Memory; //TODO
+    }
+}
